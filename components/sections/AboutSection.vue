207c2235f51955ce9fe812b15a20d7eb893724d8
Meike Jungilligens <mjung2605@users.noreply.github.com>
2025-05-28 20:53:09 +0200

    Restliche Sektionen angelegt (file only)

--- components/sections/AboutSection.vue
+++ components/sections/AboutSection.vue
@@ -9,7 +9,6 @@
     
         <p class="pa-2 ma-2">Machen Sie sich hier einen ersten Eindruck von mir!</p>
 
-        
             <v-slide-group class="pa-2 ma-2 mt-sm-16 pt-sm-8" show-arrows>
                 <v-slide-group-item
                     v-for="i in cv"
@@ -20,12 +19,8 @@
                         <p class="text-center pa-2 ma-2">{{ i.title }}</p>
                         <p class="text-center pa-2 ma-2">{{ i.place }}</p>
                     </v-col>
-                        
                 </v-slide-group-item>
             </v-slide-group>
-        
-        
-        
     </v-container>
 </template>
 

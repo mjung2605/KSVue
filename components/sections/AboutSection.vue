b7ba121e5ad721b515ad1f40f80d09824534dfd4
Meike Jungilligens <mjung2605@users.noreply.github.com>
2025-05-28 19:19:26 +0200

    Ordnerstruktur geändert

---
+++ components/sections/AboutSection.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-container class="h-screen" style="background-color: rgb(var(--v-theme-background));">
+        
+        <h2  class="pa-2 pt-sm-16 ma-2">Sie haben die Geschichte und ich die passende Stimme.</h2>
+    
+        <p class="pa-2 ma-2">Mich fasziniert es, immer wieder in neue Rollen zu schlüpfen, actiongeladene Szenen zu vertonen, oder in unentdeckte Welten einzutauchen. Gerne spreche ich aber auch ruhig, neutral und sachlich.</p>
+    
+        <p class="pa-2 ma-2">Ob Hörbuch, Hörspiel, Werbung, Synchron, Sachtext, oder Voice-Over für TV und Radio - als ausgebildete Sprecherin lasse ich ihre Vision zum Leben erwecken.</p>
+    
+        <p class="pa-2 ma-2">Machen Sie sich hier einen ersten Eindruck von mir!</p>
+
+        
+        
+        
+    </v-container>
+</template>
---
+++ components/sections/HeroSection.vue
@@ -0,0 +1,10 @@
+<template>
+    <v-container fluid class="pa-0 h-screen">
+        <v-img src="/kathi_frontview_darkened.jpg" class="h-100" cover >
+            <v-container fluid class="d-flex flex-row align-end justify-center fill-height pb-16">
+                <h1>Kathi Strikkeling</h1>
+            </v-container> 
+        </v-img>
+    </v-container>
+</template>
+

8116b1ecf9c5405abf416570314dec4627b85a4d
Meike Jungilligens <mjung2605@users.noreply.github.com>
2025-05-30 14:29:00 +0200

    Kontaktformular mit Validation gebaut

--- components/sections/ContactSection.vue
+++ components/sections/ContactSection.vue
@@ -1,5 +1,8 @@
 <template>
-    <v-container class="h-screen" style="background-color: rgb(var(--v-theme-background));">
+    <v-container class="h-screen" color="background">
         <h2 class="pa-2 pt-sm-16 pb-sm-8 ma-2">Kontaktieren Sie mich!</h2>
+        <UiContactForm/>
+
     </v-container>
 </template>
+
---
+++ components/ui/ContactForm.vue
@@ -0,0 +1,92 @@
+<template>
+    <v-form v-model="valid">
+        <v-container>
+            <v-row>
+                <v-col cols="12" md="6">
+                    <v-text-field
+                        variant="solo"
+                        v-model="firstname"
+                        :rules="textRules"
+                        label="Vorname"
+                        required>       
+                    </v-text-field>
+                </v-col>
+                <v-col cols="12" md="6">
+                    <v-text-field
+                        variant="solo"
+                        v-model="lastname"
+                        :rules="textRules"
+                        label="Nachname"
+                        required>       
+                    </v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6">
+                    <v-text-field
+                        variant="solo"
+                        v-model="email"
+                        :rules="emailRules"
+                        label="E-Mail-Adresse"
+                        required>       
+                    </v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6">
+                    <v-text-field
+                        variant="solo"
+                        v-model="tel"
+                        :rules="telRules"
+                        label="Telefonnummer">       
+                    </v-text-field>
+                </v-col>
+                <v-col cols="12">
+                    <v-textarea
+                        variant="solo"
+                        v-model="message"
+                        :rules="textRules"
+                        label="Nachricht"
+                        required>       
+                    </v-textarea>
+                </v-col>
+                <v-col class="d-flex justify-end">
+                    <v-btn  color="accent" type="submit">Absenden</v-btn>
+                </v-col>
+           </v-row>
+        </v-container>
+    </v-form>
+</template>
+
+<script>
+    export default {
+        data: () => ({ 
+            valid: false,
+            firstname: '', // empty string for value saving (separat von validation)
+            lastname: '',
+
+            // vuetify :rules erwartet Array mit Regeln
+            textRules: [
+                value => {
+                    if (value) return true;
+                    return 'Bitte füllen Sie dieses Feld aus.';
+                }
+            ],
+            email: '',
+            emailRules: [
+                value => {
+                    if (value) return true;
+                    return 'Bitte füllen Sie dieses Feld aus.';
+                },
+                value => {
+                    if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)) return true;
+                    return 'Bitte geben Sie eine gültige E-Mail-Adresse ein.'
+                }
+            ],
+            tel: '',
+            telRules: [
+                value => {
+                    if (/(\(?([\d \-\)\–\+\/\(]+){6,}\)?([ .\-–\/]?)([\d]+))/.test(value)) return true;
+                    return 'Bitte geben Sie eine gültige Telefonnummer ein (freiwillig).'
+                }
+            ],
+            message: ''
+        })
+    }
+</script>
